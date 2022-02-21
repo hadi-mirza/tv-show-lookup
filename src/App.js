0cdf3dea684d8c0b46f481baf831d33fc1ab1cb0
hadi-mirza <hadimirza1@gmail.com>
2022-02-20 20:10:27 -0500

    add api for tv show lookup and hooks for search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React from "react";
+
+const API_ENDPOINT = "http://api.tvmaze.com/search/shows?q=";
 
 function App() {
+  const showsReducer = (state, action) => {
+    switch (action.type) {
+      case "SHOWS_FETCH_INIT":
+        return {
+          ...state,
+          isLoading: true,
+          isError: false,
+        };
+      case "SHOWS_FETCH_SUCCESS":
+        return {
+          ...state,
+          isLoading: false,
+          isError: false,
+          data: action.payload,
+        };
+      case "SHOWS_FETCH_FAILURE":
+        return {
+          ...state,
+          isLoading: false,
+          isError: true,
+        };
+      default:
+        throw new Error();
+    }
+  };
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const [shows, dispatchShows] = React.useReducer(showsReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
+
+  const handleFetchShows = React.useCallback(async () => {
+    dispatchShows({ type: "SHOWS_FETCH_INIT" });
+
+    try {
+      const result = await axios.get(url);
+
+      dispatchShows({
+        type: "SHOWS_FETCH_SUCCESS",
+        payload: result.data,
+      });
+    } catch {
+      dispatchShows({ type: "SHOWS_FETCH_FAILURE" });
+    }
+  }, [url]);
+
+  React.useEffect(() => {
+    handleFetchShows();
+  }, [handleFetchShows]);
+
+  const handleSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TV Show lookup</h1>
+      <Search handleSearchTerm={handleSearchTerm} handleSearch={handleSearch} />
+      {shows.isError && <p>Something went wrong...</p>}
+      {shows.isLoading ? <p>Loading...</p> : <List list={shows.data} />}
     </div>
   );
 }
+
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.show.id} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = ({ item }) => (
+  <li>
+    <a href={item.show.url}>{item.show.name}</a>
+  </li>
+);
+
+const Search = ({ handleSearchTerm, handleSearch }) => (
+  <form onSubmit={handleSearch}>
+    <input id="search" onChange={handleSearchTerm} />
+    <button type="submit">Search</button>
+  </form>
+);
 
 export default App;
